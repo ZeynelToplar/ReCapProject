6d96751612dc4a94010cb7e7f94b8011483902d0
Zeynel Toplar <zt.2601@hotmail.com>
2021-05-27 23:58:01 +0300

    Updates

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -18,9 +18,36 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.Description.Length>=2 && car.DailyPrice>=0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Yalnızca koşullara uygun araçları sisteme ekleyebilirsiniz.\n *Araç adı en az iki karakter *Günlük Satış Fiyatı sıfırdan büyük.");
+            }
+        }
+
+        public Car Get(int id)
+        {
+            return _carDal.Get(c => c.Id == id);
+        }
+
         public List<Car> GetAll()
         {
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BranId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,12 +11,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Car car1 = new Car { Id = 1, BranId = 1, ColorId = 1, ModelYear = 2021, DailyPrice = 3500, Description = "Sıfır Araç" };
+           
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            carManager.Add(car1);
+                 
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
             }
 
+
+            // Console.WriteLine(carManager.Get().Description);
 
             Console.ReadLine();
         }
